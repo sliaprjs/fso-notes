ce5e63ebd74811ae2e25c7ca33cef35d0f0f2886
sliaprjs <sliaprjs@gmail.com>
2023-05-29 21:01:34 +0300

    move dist to backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('dist'));
+
 
 let notes = [
   {
@@ -71,7 +76,7 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end();
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
